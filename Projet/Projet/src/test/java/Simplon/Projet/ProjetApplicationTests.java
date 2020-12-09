012c119dd6794271d3b9451b354307c28d0ce258
Audrey C <audrey.cornaire@gmail.com>
2020-12-09 15:39:08 +0100

    ADD - instanciation de l'api

---
+++ Projet/Projet/src/test/java/Simplon/Projet/ProjetApplicationTests.java
@@ -0,0 +1,13 @@
+package Simplon.Projet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
